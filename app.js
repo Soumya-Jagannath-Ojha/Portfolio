5bc995516f877618fc31ae1b2db6d4f8100e188a
Soumya <soumyajagannathojha7@gmail.com>
2024-01-08 22:05:24 +0530

    Modification Done

---
+++ app.js
@@ -0,0 +1,10 @@
+// let menuLinks = document.getElementById("nav-list");
+
+// menuLinks.addEventListener("onclick",function(){
+//     toogleMenu();
+
+// });
+
+// function toogleMenu(){
+//     menuLinks.classList.toggle('show-menu');
+// }
