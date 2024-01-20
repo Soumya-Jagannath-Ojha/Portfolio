415b0dda5756363e8278b8e6e7c456edcb8fc7af
Soumya <soumyajagannathojha7@gmail.com>
2024-01-20 17:53:55 +0530

    Add Animation

--- app.js
+++ app.js
@@ -2,3 +2,16 @@ var typed = new Typed('#element', {
     strings: ['Fullstack Developer', 'MERN Stack Devloper' ,'Java Devloper'],
     typeSpeed: 80, backSpeed: 100 , backDealy: 1000, loop: true ,
   });
+
+const observer = new IntersectionObserver((entries)=>{
+  entries.forEach((entry)=>{
+    if(entry.isIntersecting){
+      entry.target.classList.add('show');
+    }else{
+      entry.target.classList.remove('show');
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el)=> observer.observe(el));
